738ffeb1f202d48054fb5fc52acc719beadff4fe
øllen <oli4.games@hotmail.com>
2021-01-14 13:43:20 +0100

    Add some borders

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "Add some borders"
 git branch -M master
-git remote add origin https://github.com/oliverwk/lngr.git
-git push -u origin master
+git remote add lngr https://github.com/oliverwk/lngr.git
+git push -u lngr master
