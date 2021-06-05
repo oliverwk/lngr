24d23862e73228fda933bbba4f47a42ec5e0331b
øllen <30601311+oliverwk@users.noreply.github.com>
2021-06-05 16:33:29 +0200

    Added widgets and loggers

--- GetCheapest/IntentHandler.swift
+++ GetCheapest/IntentHandler.swift
@@ -18,47 +18,53 @@ let logger = Logger(
 class IntentHandler: INExtension {
     override func handler(for intent: INIntent) -> Any {
         logger.log("Beginning to handle GetCheapestIntentHandler")
-        print("Handleing: \((intent as! GetCheapestIntent).value(forKey: "sort") ?? "theSort")")
-       
+        //print("Handleing: \((intent as! GetCheapestIntent).value(forKey: "sort") ?? "theSort")")
+
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
-        // let intent = IntentHelper.getCheapestIntent(for: sort)
         guard intent is GetCheapestIntent else {
                   fatalError("Unhandled intent type: \(intent)")
         }
+        
         return GetCheapestIntentHandler()
     }
 }
 
-public class GetCheapestIntentHandler: NSObject, GetCheapestIntentHandling {
-//    logger.log("Beginning to handle GetCheapestIntentHandler")
-//    Dit nog in init doen
 
+
+
+public class GetCheapestIntentHandler: NSObject, GetCheapestIntentHandling {
+    
     public func handle(intent: GetCheapestIntent, completion: @escaping (GetCheapestIntentResponse) -> Void) {
         logger.log("CheapestIntentHandler: \(intent, privacy: .public)")
+        
+        let TheLngr = TheLingerie(identifier: intent.identifier, display: "GetCheapest", pronunciationHint: "Get the cheapest")
+        //TheLingerieResolutionResult.success(with: TheLngr)
         let sort = intent.sort
         switch sort {
             case Lingeries.slip:
                 logger.log("Setting sort: \(sort.rawValue, privacy: .public)")
-                completion(GetCheapestIntentResponse.success(sort: sort))
+                // Hier userdefault of network request
+                TheLngr.prijs = 10.2
+                TheLngr.kleur = ["wit", "zwart"]
+                
+                completion(GetCheapestIntentResponse.success(sort: sort, lngr: TheLngr))
             case Lingeries.bra:
                 logger.log("Setting sort: \(sort.rawValue, privacy: .public)")
-                completion(GetCheapestIntentResponse.success(sort: sort))
+                completion(GetCheapestIntentResponse.success(sort: sort, lngr: TheLngr))
             case Lingeries.body:
                 logger.log("Setting sort: \(sort.rawValue, privacy: .public)")
-                completion(GetCheapestIntentResponse.success(sort: sort))
+                completion(GetCheapestIntentResponse.success(sort: sort, lngr: TheLngr))
             default:
                 logger.log("Setting sort: .notFound, bacause intent sort was empty")
-                //completion(GetCheapestIntentResponse.success(with: sort))
+                
                 completion(GetCheapestIntentResponse.notFound(sort: sort))
         }
         
-        //let sort: Lingeries = .slip
-        //completion(GetCheapestIntentResponse.success(sort: sort))
     }
     
     public func resolveSort(for intent: GetCheapestIntent, with completion: @escaping (GetCheapestSortResolutionResult) -> Void) {
-        logger.log("CheapestIntentHandler: \(intent, privacy: .public)")
+        logger.log("CheapestIntentHandler Resolving sort: \(intent, privacy: .public)")
         
         let sort = intent.sort
         switch intent.sort {
@@ -72,24 +78,16 @@ public class GetCheapestIntentHandler: NSObject, GetCheapestIntentHandling {
                 logger.log("Setting sort: \(sort.rawValue, privacy: .public)")
                 completion(GetCheapestSortResolutionResult.success(with: sort))
             default:
-                let sort: Lingeries = .slip
+                /*let sort: Lingeries = .slip
                 logger.log("Setting sort: .slip, bacause intent sort was empty")
-                completion(GetCheapestSortResolutionResult.success(with: sort))
-                //completion(GetCheapestSortResolutionResult.unsupported(reason: .notFound))
+                completion(GetCheapestSortResolutionResult.success(with: sort))*/
+                completion(GetCheapestSortResolutionResult.unsupported(forReason: .notFound))
         }
-        
-        /*if let sort = intent.sort {
-            logger.notice("Setting sort: \(sort.rawValue, privacy: .public)")
-            completion(GetCheapestSortResolutionResult.success(with: sort))
-        } else {
-            let sort: Lingeries = .slip
-            logger.notice("Setting sort: .slip, bacause intent sort was empty")
-            completion(GetCheapestSortResolutionResult.unsupported(reason: .notFound))
-        }*/
+    
     }
-    public func confirm(intent: GetCheapestIntent, completion: @escaping (GetCheapestIntentResponse) -> Void) {
+    /*public func confirm(intent: GetCheapestIntent, completion: @escaping (GetCheapestIntentResponse) -> Void) {
         let sort: Lingeries = .slip
         completion(GetCheapestIntentResponse.success(sort: sort))
-    }
+    }*/
 }
         
