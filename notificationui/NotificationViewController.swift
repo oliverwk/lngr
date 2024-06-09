67e88c0811a17cb1ae2a4ff96051909814aa7a9d
øllen <30601311+oliverwk@users.noreply.github.com>
2024-06-09 13:10:24 +0200

    fixed notifaction

--- notificationui/NotificationViewController.swift
+++ notificationui/NotificationViewController.swift
@@ -19,24 +19,28 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     @IBOutlet var label: UILabel?
     @IBOutlet weak var notiImage: UIImageView?
     @IBOutlet weak var colorSelector: UISegmentedControl?
+    @IBOutlet weak var imageStepper: UIStepper!
+    
     var urls = [URL(string: "https://www.na-kd.com/resize/globalassets/nakd_classic_cotton_thong-1013-000820-0138_01j.jpg?width=640"),
                 URL(string: "https://www.na-kd.com/resize/globalassets/nakd_classic_cotton_thong-1013-000820-0138_02i.jpg?width=640"),
                 URL(string: "https://www.na-kd.com/resize/globalassets/nakd_classic_cotton_thong-1013-000820-0138_03h.jpg?width=640"),
                 URL(string: "https://www.na-kd.com/resize/globalassets/nakd_classic_cotton_thong-1013-000820-0138_04k.jpg?width=640")]
+    var kleurFams = [KleurFamilie]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any required interface initialization here.
         logger.log("the view: \(self.view.debugDescription, privacy: .public)")
         colorSelector?.addTarget(self, action: #selector(colorSelectorTapped(sender:)), for: .valueChanged)
+        imageStepper.addTarget(self, action: #selector(imageSelectorTappedStepper(sender:)), for: .valueChanged)
     }
     
-    @objc func colorSelectorTapped(sender: UISegmentedControl) {
+    func getImage(url: URL) {
         self.notiImage?.image = UIImage(named: "String")
         let networkConnctions = true
         if networkConnctions {
             logger.log("urls \(self.urls)")
-            let task = URLSession.shared.dataTask(with: (self.urls[sender.selectedSegmentIndex] ?? URL(string: "https://www.na-kd.com/resize/globalassets/nakd_classic_cotton_thong-1013-000820-0138_04k.jpg?width=640"))!) {(data, response, error) in
+            let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
                 guard let data = data else { return }
                 if (error != nil) {
                     self.logger.log("The error: \(error.debugDescription, privacy: .public)")
@@ -45,7 +49,6 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
                     self.logger.log("The res: \(code ?? 0, privacy: .public)")
                     DispatchQueue.main.async {
                         self.notiImage?.image = UIImage(data: data)
-                        
                         self.logger.log("\((Int((self.notiImage?.image!.size.width)!) / Int((self.notiImage?.image?.size.height)!))*390)")
                     }
                 }
@@ -53,16 +56,56 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
             task.resume()
         }
     }
+                                                  
+    @objc func colorSelectorTapped(sender: UISegmentedControl) {
+        print("Tapped index at colour: \(sender.selectedSegmentIndex)")
+        getImage(url: self.kleurFams[sender.selectedSegmentIndex].imgURL)
+        Task {
+            print("Dit een andere lngr, waar wij geen info over hebben, dus nieuwe aan het halen zijn")
+            self.urls = await getExtraImages(searchUrl: self.kleurFams[sender.selectedSegmentIndex].url) ?? []
+            imageStepper.maximumValue = Double(self.urls.count)-1
+        }
+    }
+    
+    @objc func imageSelectorTappedStepper(sender: UIStepper) {
+        print("Tapped index at imageStepper: \(sender.value)")
+        getImage(url: (self.urls[Int(sender.value)] ?? URL(string: "https://www.na-kd.com/resize/globalassets/nakd_classic_cotton_thong-1013-000820-0138_04k.jpg?width=640"))!)
+    }
+
+    func getExtraImages(searchUrl: URL) async -> [URL]? {
+        let url = URL(string: "https://nkd_worker.wttp.workers.dev/getImagesColour/"+searchUrl.absoluteString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)!
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodeSet = try JSONDecoder().decode([String].self, from: data)
+            var ImageUrlS = [URL]()
+            for url in decodeSet {
+                ImageUrlS.append(URL(string: url)!)
+            }
+            return ImageUrlS
+        } catch {
+            self.logger.fault("[ERROR] Er was geen data met het laden een url: \(url.absoluteString, privacy: .public) Met de error: \(String(describing: error), privacy: .public)")
+            return nil
+        }
+    }
     
     func didReceive(_ notification: UNNotification) {
-       
-        
-        if let kleurFamilies = notification.request.content.userInfo["kleurFamilies"] as? Array<String> {
-            for (i, kleur) in kleurFamilies.enumerated()  {
-                colorSelector?.setTitle(kleur, forSegmentAt: i)
-                logger.log("setTitle to \(kleur, privacy: .public) at \(i, privacy: .public)")
+        if let kleurFamiliesData = notification.request.content.userInfo["kleurFamilies"] as? String {
+            let kleurFamilies = try? JSONDecoder().decode([KleurFamilie].self, from: Data(kleurFamiliesData.utf8))
+            self.kleurFams = kleurFamilies ?? []
+            var i = 0
+            if kleurFamilies?.count ?? 1 > colorSelector?.numberOfSegments ?? 2 {
+                for irs in 0...((kleurFamilies?.count ?? 1)-(colorSelector?.numberOfSegments ?? 2)) {
+                    colorSelector?.insertSegment(withTitle: "kleur \(irs)", at: irs + (colorSelector?.numberOfSegments ?? 2), animated: true)
+                }
             }
-            colorSelector?.selectedSegmentIndex = 3
+            
+            for kleur in kleurFamilies ?? []  {
+                colorSelector?.setTitle(kleur.naam, forSegmentAt: i)
+                logger.log("setTitle to \(kleur.naam, privacy: .public) at \(i, privacy: .public)")
+                i += 1
+            }
+            colorSelector?.selectedSegmentIndex = 0
         } else {
             logger.log("no kleurFamilies in userinfo")
         }
@@ -80,7 +123,8 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
                 ImageUS.append(URL(string: url)!)
             }
             self.urls = ImageUS
-        }else {
+            imageStepper.maximumValue = Double(self.urls.count)-1
+        } else {
             logger.log("no imageURLS in userinfo")
         }
         
