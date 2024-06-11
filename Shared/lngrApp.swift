39998bc8279c42a05365319d5decbc055cf0e404
øllen <30601311+oliverwk@users.noreply.github.com>
2024-06-11 23:02:11 +0200

    - Updated for macos - removed lagacy code

--- Shared/lngrApp.swift
+++ Shared/lngrApp.swift
@@ -21,7 +21,9 @@ struct lngrApp: App {
         subsystem: "nl.wittopkoning.lngr",
         category: "lngrApp"
     )
+    #if os(iOS)
     private var backgroundSupport = BackgroundSupport()
+    #endif
     @State private var openingWidget = false
     @State private var rndint: Int = 0
     
@@ -41,16 +43,20 @@ struct lngrApp: App {
             } else {
                 ContentView()
                     .onOpenURL { url in
+                        #if os(iOS)
                         if url.scheme == "vacinn-widget" {
                             self.logger.log("Opening: \"https://coronadashboard.rijksoverheid.nl/landelijk/vaccinaties\" beacuse the url scheme was: vacinn-widget")
                             if let theUrl = URL(string: "https://coronadashboard.rijksoverheid.nl/landelijk/vaccinaties") {
                                 UIApplication.shared.open(theUrl)
                             }
-                        } else if url.scheme == "stoic-widget" {
+                        }
+                        #endif
+                        if url.scheme == "stoic-widget" {
                             openingWidget = true
                             self.rndint = Int(url.absoluteString.split(separator: "://")[1]) ?? 0
                         }
                     }
+                #if os(iOS)
                     .onAppear {
                         BGTaskScheduler.shared.register(forTaskWithIdentifier: "nl.wittopkoning.lngr.GetNewLngrTask", using: nil) { (task) in
                             backgroundSupport.handleAppRefreshTask(task: task as! BGAppRefreshTask)
@@ -59,37 +65,9 @@ struct lngrApp: App {
                     .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
                         backgroundSupport.scheduleAppRefresh()
                     }
+                #endif
             }
            
-        }
-    }
-    
-    func handleSpotlight(_ userActivity: NSUserActivity) {
-        self.logger.log("[SPOTLIGHT] Opend a spotlight link")
-        //let defaults = UserDefaults.standard
-        let defaults = UserDefaults(suiteName: "nl.wittopkoning.lngr.lngrs")!
-        self.logger.log("userInfo: \(userActivity.userInfo.debugDescription, privacy: .public)")
-        if let id = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String {
-            self.logger.log("[SPOTLIGHT] Found identifier \(id, privacy: .public)")
-            for lng in ["lngrSlips", "lngrBodys"] {
-                if let savedlngr = defaults.data(forKey: lng) as Data? {
-                    self.logger.log("savedlngr: \(savedlngr, privacy: .public)")
-                    if let loadedLngr = try? JSONDecoder().decode([Lingerie].self, from: savedlngr) {
-                        self.logger.log("[SPOTLIGHT] Is loadedLngr an array: \(loadedLngr[0], privacy: .public), hopelijk is dit een id: \(loadedLngr[0].id, privacy: .public)")
-                        for theLngr in loadedLngr {
-                            if theLngr.id == id {
-                                self.logger.log("[SPOTLIGHT] Found \(id, privacy: .public) with name \(theLngr.naam, privacy: .public)")
-                            }
-                        }
-                    } else {
-                        self.logger.fault("Failed to parse the json from UserDefaults")
-                    }
-                } else {
-                    self.logger.fault("Failed to get lngr from UserDefaults")
-                }
-            }
-        } else {
-            self.logger.fault("Failed to get lngr id from the spotlight link")
         }
     }
 }
